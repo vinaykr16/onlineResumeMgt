c184644df819c78c0bf1a0a6685ea05319d267c6
vinaykr16 <vinaykr16121988@gmail.com>
2025-07-10 11:45:43 +0530

    changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,8 +46,14 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
+
+                                <a class="btn btn-link" href="{{ route('register') }}">
+                                        {{ __('Register') }}
+                                </a>
                             </div>
                         </div>
+
+
                     </form>
                 </div>
             </div>
